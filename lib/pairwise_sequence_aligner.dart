8a7ec82b4900b8c67d098f3937c19b084084ff44
ManaRegen <132016620+ManaRegen@users.noreply.github.com>
2024-10-04 10:03:54 -0700

    feat: implement traceback and alignSequences

--- lib/pairwise_sequence_aligner.dart
+++ lib/pairwise_sequence_aligner.dart
@@ -13,6 +13,7 @@ class PairwiseSequenceAligner {
 
   static List<List<MatrixCell>> generateScoringMatrix(
       String sequence1, String sequence2) {
+    // Initialize the matrix with the correct dimensions
     List<List<MatrixCell>> matrix = List.generate(
       sequence1.length + 1,
       (i) => List.generate(
@@ -22,18 +23,20 @@ class PairwiseSequenceAligner {
     );
 
     // Fill in the first row and column of the matrix
-    for (int i = 0; i < sequence1.length + 1; i++) {
+    for (int i = 1; i < sequence1.length + 1; i++) {
       matrix[i][0].value = i * ScoringScheme.gap;
+      matrix[i][0].paths!.add(Index(i: i - 1, j: 0));
     }
 
-    for (int j = 0; j < sequence2.length + 1; j++) {
+    for (int j = 1; j < sequence2.length + 1; j++) {
       matrix[0][j].value = j * ScoringScheme.gap;
+      matrix[0][j].paths!.add(Index(i: 0, j: j - 1));
     }
 
     return matrix;
   }
 
-  SequenceAlignment alignSequences() {
+  List<SequenceAlignment> alignSequences() {
     // Fill in the rest of the matrix
     for (int i = 1; i < sequence1.length + 1; i++) {
       for (int j = 1; j < sequence2.length + 1; j++) {
@@ -41,10 +44,9 @@ class PairwiseSequenceAligner {
       }
     }
 
-    int finalScore = matrix[sequence1.length + 1][sequence2.length + 1].value;
-    String alignedSequence1 = "";
-    String alignedSequence2 = "";
-    return SequenceAlignment(alignedSequence1, alignedSequence2, finalScore);
+    List<SequenceAlignment> alignments =
+        traceback(sequence1.length, sequence2.length);
+    return alignments;
   }
 
   MatrixCell calculateCell(int i, int j) {
@@ -52,6 +54,7 @@ class PairwiseSequenceAligner {
     MatrixCell northCell = matrix[i - 1][j];
     MatrixCell westCell = matrix[i][j - 1];
 
+    // calculate scores for the three possible paths to the current cell
     int northwestCellScore = northwestCell.value +
         (sequence1[i - 1] == sequence2[j - 1]
             ? ScoringScheme.match
@@ -59,11 +62,12 @@ class PairwiseSequenceAligner {
     int northCellScore = northCell.value + ScoringScheme.gap;
     int westCellScore = westCell.value + ScoringScheme.gap;
 
+    // find the highest score
     List<int> scores = [northwestCellScore, northCellScore, westCellScore];
     int maxScore = scores.reduce(max);
 
     List<Index> paths = [];
-
+    // add all optimal paths to the current cell
     if (northwestCellScore == maxScore) {
       paths.add(Index(i: i - 1, j: j - 1));
     }
@@ -77,16 +81,47 @@ class PairwiseSequenceAligner {
     return MatrixCell(value: maxScore, paths: paths);
   }
 
-  List<SequenceAlignment> traceback() {
-    //TODO: Convert return type to a list of alignments
-    List<SequenceAlignment> alignedSequences;
+  List<SequenceAlignment> traceback(int i, int j) {
+    List<SequenceAlignment> currentAlignments = [];
 
-    Index currentIndex = Index(i: sequence1.length, j: sequence2.length);
-    while (currentIndex.i > 0 || currentIndex.j > 0) {
-      MatrixCell currentCell = matrix[currentIndex.i][currentIndex.j];
-      //TODO: Generate an alignment for each path in the current cell
+    MatrixCell currentCell = matrix[i][j];
+    // Base case
+    if (currentCell.paths == null || currentCell.paths!.isEmpty) {
+      currentAlignments.add(SequenceAlignment("", "", 0));
+      return currentAlignments;
     }
 
-    return alignedSequences;
+    for (var path in currentCell.paths!) {
+      if (path.i == i - 1 && path.j == j - 1) {
+        // Get and append to alignments from northwest cell
+        List<SequenceAlignment> previousAlignments = traceback(path.i, path.j);
+        for (var alignment in previousAlignments) {
+          alignment.alignedSequence1 += sequence1[i - 1];
+          alignment.alignedSequence2 += sequence2[j - 1];
+          alignment.score = currentCell.value;
+        }
+        currentAlignments.addAll(previousAlignments);
+      } else if (path.i == i - 1 && path.j == j) {
+        // Get and append to alignments from north cell
+        List<SequenceAlignment> previousAlignments = traceback(path.i, path.j);
+        for (var alignment in previousAlignments) {
+          alignment.alignedSequence1 += sequence1[i - 1];
+          alignment.alignedSequence2 += "-";
+          alignment.score = currentCell.value;
+        }
+        currentAlignments.addAll(previousAlignments);
+      } else if (path.i == i && path.j == j - 1) {
+        // Get and append to alignments from west cell
+        List<SequenceAlignment> previousAlignments = traceback(path.i, path.j);
+        for (var alignment in previousAlignments) {
+          alignment.alignedSequence1 += "-";
+          alignment.alignedSequence2 += sequence2[j - 1];
+          alignment.score = currentCell.value;
+        }
+        currentAlignments.addAll(previousAlignments);
+      }
+    }
+
+    return currentAlignments;
   }
 }
